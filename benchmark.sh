9dc81345fc6edb2044910f9a711b3d17b34c5564
Michael Ruppert <mr@atr0x.com>
2019-08-19 22:13:38 +0200

    hmm

--- benchmark.sh
+++ benchmark.sh
@@ -2,4 +2,6 @@
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 /usr/bin/env python3 -m venv $tmp_dir
 source $tmp_dir/bin/activate
-pip install -U setuptools wheel git+https://github.com/miweru/benchmark
+pip install -U setuptools wheel
+pip install somajo==1.10.5 scikit-learn==0.21.3 scipy==1.3.1
+pip install git+https://github.com/miweru/benchmark
