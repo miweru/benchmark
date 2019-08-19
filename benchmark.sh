d0c531918b526596e13df2e25bb8b9ad3309f444
Michael Ruppert <mr@atr0x.com>
2019-08-19 22:31:36 +0200

    easyer

--- benchmark.sh
+++ benchmark.sh
@@ -4,4 +4,6 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 source $tmp_dir/bin/activate
 pip install -U setuptools wheel
 pip install somajo==1.10.5 scikit-learn==0.21.3 scipy==1.3.1
-pip install git+https://github.com/miweru/benchmark
+cd $tmp_dir
+wget https://raw.githubusercontent.com/miweru/benchmark/master/pybenchmark
+python3 pybenchmark
